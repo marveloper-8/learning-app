1893f0342a8103f5be5dc5ea2eeb2ba7456581b9
Josh <marveloper.8@gmail.com>
2024-11-22 23:19:02 -0800

    subjects controller

--- backend/src/modules/subjects/subjects.controller.ts
+++ backend/src/modules/subjects/subjects.controller.ts
@@ -1,4 +1,30 @@
-import { Controller, UseGuards } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post, UseGuards } from "@nestjs/common";
+import { JwtAuthGuard } from "src/guards/jwt-auth.guard";
+import { SubjectsService } from "./subjects.service";
+import { CreateSubjectDto } from "./dto/create-subject.dto";
 
 @Controller('subjects')
-@UseGuards(JwtAu)
+@UseGuards(JwtAuthGuard)
+export class SubjectsController {
+    constructor(private readonly subjectsService: SubjectsService) {}
+
+    @Get()
+    async getAllSubjects() {
+        return this.subjectsService.getAllSubjects();
+    }
+
+    @Get(':id')
+    async getSubject(@Param('id') id: string) {
+        return this.subjectsService.getSubject(Number(id));
+    }
+
+    @Get(':id/leaderboard')
+    async getLeaderboard(@Param('id') id: string) {
+        return this.subjectsService.getSubjectLeaderboard(Number(id));
+    }
+
+    @Post()
+    async createSubject(@Body() createSubjectDto: CreateSubjectDto) {
+        return this. subjectsService.createSubject(createSubjectDto)
+    }
+}
